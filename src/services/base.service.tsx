1354a3556450191242c61dd3acbda49182b9bedf
WellingtonFidelis <wellingtonf20@gmail.com>
2021-03-18 22:03:03 -0300

    adding new route for test request API

--- src/services/base.service.tsx
+++ src/services/base.service.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://172.17.0.2:8000/api/form-test/",
+  baseURL: "http://127.0.0.1:8000/api/form-test/",
+  // baseURL: "http://172.17.0.2:8000/api/form-test/",
 });
 
 export default api;
