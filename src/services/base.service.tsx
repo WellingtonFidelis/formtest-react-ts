62eabfca11366aa96e4653b38faa080402f96c59
Ubuntu <ubuntu@ip-172-31-26-136.us-east-2.compute.internal>
2021-04-05 23:31:27 +0000

    adding files to config the Rest and deploy on aws vps

--- src/services/base.service.tsx
+++ src/services/base.service.tsx
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "http://127.0.0.1:8000/api/form-test/",
-  baseURL: "http://172.17.0.2:8000/api/form-test/",
+//	baseURL: "http://127.0.0.1:8000/api/form-test/",
+//	baseURL: "http://172.17.0.2:8000/api/form-test/",
+	baseURL: "http://3.23.94.83:8000/api/form-test/",
 });
 
 export default api;
